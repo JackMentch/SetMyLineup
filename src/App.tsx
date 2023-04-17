5494128be0b214b5af26e2a2c1954fadc5793eac
jackmentch <j.mentch@icloud.com>
2023-04-17 09:05:44 -0400

    name

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
 
   return (
     <div className="App" style={{ backgroundColor: colorScheme[0], height: showStats ? '135vh' : '100vh' }}>
-      <h1>3up3down.app</h1>
+      <h1>SetMyLineup.App</h1>
       {/* <p className="title">Set My Lineup</p> */}
 
       <button className="btn btn-blue view-stats" onClick={() => setShowStats((!showStats))}>
